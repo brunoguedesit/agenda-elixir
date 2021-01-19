1201134fde76f459ac94bbf7da937972c401475b
Bruno Guedes <brunoguedesit@gmail.com>
2021-01-19 00:48:06 -0300

    Subindo sistema de agenda escrito em Elixir

---
+++ lib/mix/tasks/utils/add_fake_friends.ex
@@ -0,0 +1,39 @@
+defmodule Mix.Tasks.Utils.AddFakeFriends do
+  use Mix.Task
+  alias NimbleCSV.RFC4180, as: CSVParser
+  alias FriendsApp.CLI.Friend
+
+  @shortdoc "Add Fake Friends on App"
+  def run(_) do
+    Faker.start()
+
+    create_friends([], 50)
+    |> CSVParser.dump_to_iodata
+    |> save_csv_file
+
+    IO.puts("Amigos cadastrados com sucesso!!!")
+  end
+
+  defp create_friends(list, count)  when count <= 1 do
+    list ++ [random_list_friends()]
+  end
+
+  defp create_friends(list, count) do
+    list ++ [random_list_friends()] ++ create_friends(list, count - 1)
+  end
+
+  defp random_list_friends do
+    %Friend{
+      name: Faker.Person.PtBr.name(),
+      email: Faker.Internet.email(),
+      phone: Faker.Phone.EnUs.phone()
+    }
+    |> Map.from_struct()
+    |> Map.values()
+  end
+
+  defp save_csv_file(data) do
+    Application.fetch_env!(:friends_app, :csv_file_path)
+    |> File.write!(data, [:append])
+  end
+end
